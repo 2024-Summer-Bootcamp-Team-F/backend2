cddcd69d52fdde3b329e0165889a29e9327783db
Gwanghyeon-k <hsys06207@gmail.com>
2024-06-30 22:51:19 +0900

    feat: 백엔드 초기 세팅
    node express, mysql, swagger 세팅

--- server/app.js
+++ server/app.js
@@ -1,8 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const port = 8000;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!!!!!!!!!!!!!!!!!!!!!!!')
-})
+app.use(express.json());
 
-app.listen(8000)
+// 기본 라우트 설정
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+//swagger 모듈 참조하기
+const { swaggerUi, specs } = require("./swagger/swagger");
+
+//swagger 모듈 호출하기
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs));
+
+//사용자 라우터 연결
+const usersRouter = require("./routes/users");
+app.use('/users', usersRouter);
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
